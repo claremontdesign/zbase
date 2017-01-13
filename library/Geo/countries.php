4ed7f07720f049bc9761943fc65635afc33a05c6
Dennes B Abing <dennes.b.abing@gmail.com>
2017-01-13 20:40:37 +0800

    Zbase updates

--- library/Geo/countries.php
+++ library/Geo/countries.php
@@ -1,9 +1,9 @@
 <?php
 
-$countries = __DIR__ . '/countriesData.php';
+$countries = require __DIR__ . '/countriesData.php';
 $data = [];
 foreach ($countries as $countryCode => $country)
 {
-	$data[] = [$countryCode => $country['name']];
+	$data[$countryCode] = $country['name'];
 }
 return $data;
